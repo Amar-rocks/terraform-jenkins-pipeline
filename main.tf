321a3c1458dca3f67ff8a81aa6a8c75181f1b532
Amar-rocks <amar.deshmukh666@gmail.com>
2025-06-28 15:42:16 +1000

    Changing AMI ID

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-078772dab3242ee11"
+  ami = "ami-03cc0414805179cc7"
   instance_type = "t2.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
