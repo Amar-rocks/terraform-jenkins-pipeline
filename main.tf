6a2b6bc26765346fedb6eb0b6d5f321e0b42fc87
Amar-rocks <amar.deshmukh666@gmail.com>
2025-06-28 15:18:06 +1000

    Fixing instance type

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
   ami = "ami-078772dab3242ee11"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
     "Name" = "EC2 Demo"
