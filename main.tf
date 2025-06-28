f840be3a214ee85c366ce513841396658def2958
Amar-rocks <amar.deshmukh666@gmail.com>
2025-06-28 15:49:41 +1000

    AMI ID chnage

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-03cc0414805179cc7"
+  ami = "ami-078772dab3242ee11"
   instance_type = "t2.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
